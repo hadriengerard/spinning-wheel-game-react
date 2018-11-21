846f21809643efe18a171495aa3c387816655001
Hadrien Gérard <hadriengerard@users.noreply.github.com>
2018-11-21 01:13:42 +0100

    Moved wheel to a separate component

---
+++ src/components/wheel/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import './index.css';
+
+export default class Wheel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItem: null,
+    };
+    this.selectItem = this.selectItem.bind(this);
+  }
+
+  selectItem() {
+    if (this.state.selectedItem === null) {
+      const selectedItem = Math.floor(Math.random() * this.props.items.length);
+      if (this.props.onSelectItem) {
+        this.props.onSelectItem(selectedItem);
+      }
+      this.setState({ selectedItem });
+    } else {
+      this.setState({ selectedItem: null });
+      setTimeout(this.selectItem, 500);
+    }
+  }
+
+  render() {
+    const { selectedItem } = this.state;
+    const { items } = this.props;
+
+    const wheelVars = {
+      '--nb-item': items.length,
+      '--selected-item': selectedItem,
+    };
+    const spinning = selectedItem !== null ? 'spinning' : '';
+
+    return (
+      <div className="wheel-container">
+        <div className={`wheel ${spinning}`} style={wheelVars} onClick={this.selectItem}>
+          {items.map((item, index) => (
+            <div className="wheel-item" key={index} style={{ '--item-nb': index }}>
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,49 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Wheel from './components/wheel';
+
 import './styles.css';
 
 export class App extends React.Component {
   constructor() {
     super();
     this.places = ['Pizzas', 'Sandwiches', 'Salads', 'Soup', 'Japanese food', 'Pastas'];
-    this.state = {
-      selectedItem: null,
-    };
-    this.selectItem = this.selectItem.bind(this);
-  }
-
-  selectItem() {
-    if (this.state.selectedItem === null) {
-      this.setState({
-        selectedItem: Math.floor(Math.random() * this.places.length),
-      });
-    } else {
-      this.setState({ selectedItem: null });
-      setTimeout(this.selectItem, 500);
-    }
   }
 
   render() {
-    const { selectedItem } = this.state;
-    const wheelVars = {
-      '--nb-item': this.places.length,
-      '--selected-item': selectedItem,
-    };
-    const spinning = selectedItem !== null ? 'spinning' : '';
-
     return (
       <div className="App">
         <h1>What should you eat today?</h1>
-        <div className="wheel-container">
-          <div className={`wheel ${spinning}`} style={wheelVars} onClick={this.selectItem}>
-            {this.places.map((place, index) => (
-              <div className="wheel-item" key={index} style={{ '--item-nb': index }}>
-                {place}
-              </div>
-            ))}
-          </div>
-        </div>
+        <Wheel items={this.places} />
       </div>
     );
   }
